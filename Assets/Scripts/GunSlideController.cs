0955f2c93b0a5ea772a3c85a4d809debffccc5c1
James Billy <james@epicguru.co.uk>
2019-10-17 23:03:52 +0200

    Adding animations. Still missing: Crouch layer, inspect, equip.

---
+++ Assets/Scripts/GunSlideController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class GunSlideController : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 ClosedPos, OpenPos;
+    [Range(0f, 1.5f)]
+    public float OpenLerp = 0f;
+
+    [Header("Code editing only!")]
+    [Range(0f, 1.5f)]
+    public float OverrideLerp = 0f;
+
+    public bool AnimDriven = true;
+
+    private void LateUpdate()
+    {
+        Target.localPosition = Vector3.LerpUnclamped(ClosedPos, OpenPos, AnimDriven ? OpenLerp : OverrideLerp);
+    }
+}
