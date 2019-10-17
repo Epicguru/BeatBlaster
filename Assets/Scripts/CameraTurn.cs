fabc474f0903c64163650966c72ddbb37135a62d
James Billy <james@epicguru.co.uk>
2019-10-17 21:32:43 +0200

    Adding a pimped out glock.

---
+++ Assets/Scripts/CameraTurn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraTurn : MonoBehaviour
+{
+    public float Sensitvity = 0.5f;
+
+    [Header("References")]
+    public Transform Horizontal;
+    public Transform Vertical;
+
+    private float verticalAngle;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float cX = Input.GetAxisRaw("Mouse X");
+        float cY = Input.GetAxisRaw("Mouse Y");
+
+        Horizontal.Rotate(0f, cX * Sensitvity, 0f, Space.Self);
+        verticalAngle -= cY;
+        verticalAngle = Mathf.Clamp(verticalAngle, -90f, 90f);
+        Vertical.localEulerAngles = new Vector3(verticalAngle, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController Controller;
+
+    [Header("Movement")]
+    public float GravityForce = 9.81f;
+    public Vector3 Velocity;
+    public bool OnFloor = false;
+    public float JumpVelocity = 5f;
+
+    private void Update()
+    {
+        if(OnFloor && Input.GetKeyDown(KeyCode.Space))
+        {
+            Velocity.y = JumpVelocity;
+        }
+
+        Vector3 gravity = new Vector3(0f, GravityForce, 0f);
+        Velocity += gravity * Time.deltaTime;
+        var flags = Controller.Move(Velocity * Time.deltaTime);
+
+        OnFloor = false;
+        if (flags.HasFlag(CollisionFlags.Below))
+        {
+            Velocity.y = -0.1f;
+            OnFloor = true;
+        }
+    }
+}
